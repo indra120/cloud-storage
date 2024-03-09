23b02302342ac9ec2fc6eebe046cda1f9a00daee
indra120 <indrapranata120@gmail.com>
2024-03-09 11:40:48 +0800

    Update

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://poetic-salmon-66.clerk.accounts.dev",
+      domain: "https://ruling-dory-56.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
