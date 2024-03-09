5d7240119ec9789192f1ec08f2a12c5777e6be4e
indra120 <indrapranata120@gmail.com>
2024-03-09 11:56:31 +0800

    Update

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,7 +4,8 @@ import { v } from "convex/values";
 export const fileTypes = v.union(
   v.literal("image"),
   v.literal("csv"),
-  v.literal("pdf")
+  v.literal("pdf"),
+  v.literal("mp4")
 );
 
 export const roles = v.union(v.literal("admin"), v.literal("member"));
